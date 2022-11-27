95a7c7188bd2fee7454169f8916b4a48c2c4fc05
ludovic-str <ludovic.sutter@epitech.eu>
2022-11-27 12:39:21 +0100

    Feat: add base on all day

---
+++ create-folder.sh
@@ -0,0 +1,5 @@
+for i in {1..24}
+do
+   mkdir "day$i"
+   cp solver.js "day$i/"
+done
---
+++ day18/solver.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+const buff = fs.readFileSync("./input", "utf-8");
+
+const lines = buff.split("\n");
