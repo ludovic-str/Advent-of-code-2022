c3fba8b4e1e2c7c4c5b8d41b01569b839f3b91d0
ludovic-str <ludovic.sutter@epitech.eu>
2022-12-01 07:45:27 +0100

    Feat: finish d1

--- day1/solver.js
+++ day1/solver.js
@@ -3,3 +3,19 @@ const fs = require("fs");
 const buff = fs.readFileSync("./input", "utf-8");
 
 const lines = buff.split("\n");
+
+const elvesFood = [];
+let tmp = 0;
+
+for (const elem of lines) {
+  if (elem.length !== 0) {
+    tmp += parseInt(elem);
+  } else {
+    elvesFood.push(tmp);
+    tmp = 0;
+  }
+}
+
+elvesFood.sort((a, b) => b - a);
+
+console.log(elvesFood[0] + elvesFood[1] + elvesFood[2]);
