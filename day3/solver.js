0b27c902e7be376ed503f91b9c58834752a3f32c
ludovic-str <ludovic.sutter@epitech.eu>
2022-12-03 14:43:56 +0100

    Feat: 100% day 3

--- day3/solver.js
+++ day3/solver.js
@@ -3,3 +3,52 @@ const fs = require("fs");
 const buff = fs.readFileSync("./input", "utf-8");
 
 const lines = buff.split("\n");
+
+const ex1 = () => {
+  let total = 0;
+
+  for (const item of lines) {
+    const middle = item.length / 2;
+    const s1 = item.slice(0, middle).split("");
+    const s2 = item.slice(middle).split("");
+    for (const char of s1) {
+      if (s2.includes(char)) {
+        total +=
+          char === char.toLocaleUpperCase()
+            ? char.charCodeAt(0) - 65 + 27
+            : char.charCodeAt(0) - 96;
+        break;
+      }
+    }
+  }
+
+  return total;
+};
+
+const ex2 = () => {
+  let total = 0;
+  let groups = [];
+  let tmp = [];
+  for (const item of lines) {
+    tmp.push(item);
+    if (tmp.length === 3) {
+      groups.push(tmp);
+      tmp = [];
+    }
+  }
+
+  for (const group of groups) {
+    for (const char of group[0]) {
+      if (group[1].includes(char) && group[2].includes(char)) {
+        total +=
+          char === char.toLocaleUpperCase()
+            ? char.charCodeAt(0) - 65 + 27
+            : char.charCodeAt(0) - 96;
+        break;
+      }
+    }
+  }
+  console.log(total);
+};
+
+ex2();
