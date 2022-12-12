9e04950fc444fda0991a46dd4f12b80d1ef329d7
ludovic-str <ludovic.sutter@epitech.eu>
2022-12-12 22:42:01 +0100

    Backup

--- day10/solver.js
+++ day10/solver.js
@@ -3,3 +3,35 @@ const fs = require("fs");
 const buff = fs.readFileSync("./input", "utf-8");
 
 const lines = buff.split("\n");
+let cicles = 0;
+let x = 1;
+let i = 0;
+let queue = [];
+let sum = 0;
+const mainCicles = [20, 60, 100, 140, 180, 220];
+let crt = "";
+const crtTab = [];
+
+while (i < lines.length) {
+  cicles++;
+  crt += crt.length >= x - 1 && crt.length <= x + 1 ? "#" : ".";
+  if (crt.length === 40) {
+    crtTab.push(crt);
+    crt = "";
+  }
+  if (mainCicles.includes(cicles)) {
+    sum += x * cicles;
+  }
+  if (queue.length !== 0) {
+    x += queue[0];
+    queue.pop();
+    i++;
+    continue;
+  }
+  const splitedLine = lines[i].split(" ");
+  if (splitedLine[0] === "noop") i++;
+  else if (splitedLine[0] === "addx") queue.push(parseInt(splitedLine[1]));
+}
+
+console.log(crtTab);
+console.log(sum);
--- day8/solver.js
+++ day8/solver.js
@@ -3,3 +3,78 @@ const fs = require("fs");
 const buff = fs.readFileSync("./input", "utf-8");
 
 const lines = buff.split("\n");
+
+let total = 0;
+
+const isTop = (posI, posJ) => {
+  let high = parseInt(lines[posI][posJ]);
+  let up = 0;
+  let total = 0;
+  for (let i = posI + 1; i <= lines.length - 1; i++) {
+    total++;
+    if (high <= parseInt(lines[i][posJ])) {
+      up = 1;
+      break;
+    }
+  }
+  return total;
+};
+
+const isBottom = (posI, posJ) => {
+  let high = parseInt(lines[posI][posJ]);
+  let down = 0;
+  let total = 0;
+  for (let i = posI - 1; i >= 0; i--) {
+    total++;
+    if (high <= parseInt(lines[i][posJ])) {
+      down = 1;
+      break;
+    }
+  }
+  return total;
+};
+
+const isLeft = (posI, posJ) => {
+  let high = parseInt(lines[posI][posJ]);
+  let down = 0;
+  let total = 0;
+  for (let j = posJ - 1; j >= 0; j--) {
+    total++;
+    if (high <= parseInt(lines[posI][j])) {
+      down = 1;
+      break;
+    }
+  }
+  return total;
+};
+
+const isRight = (posI, posJ) => {
+  let high = parseInt(lines[posI][posJ]);
+  let up = 0;
+  let total = 0;
+  for (let j = posJ + 1; j <= lines.length - 1; j++) {
+    total++;
+    if (high <= parseInt(lines[posI][j])) {
+      up = 1;
+      break;
+    }
+  }
+  return total;
+};
+
+for (let i = 0; lines[i]; i++) {
+  for (let j = 0; lines[i][j]; j++) {
+    if (
+      i === 0 ||
+      j === 0 ||
+      j === lines[i].length - 1 ||
+      i === lines.length - 1
+    )
+      continue;
+    const tree = [isTop(i, j), isBottom(i, j), isLeft(i, j), isRight(i, j)];
+    if (tree[0] * tree[1] * tree[2] * tree[3] > total) {
+      total = tree[0] * tree[1] * tree[2] * tree[3];
+    }
+  }
+}
+console.log(total);
